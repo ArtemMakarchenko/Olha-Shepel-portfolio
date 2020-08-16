8c79c7b0f2b9d9aea6ba603dc86857db065ed43b
makarich98 <artem1998makarchenko@gmail.com>
2020-08-16 23:54:16 +0300

    Ceated main page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Скрипт подключен");
+let logoText = document.querySelector("h1");
+
+function rideFromRight(){
+    logoText.style.opacity = 1;
+    logoText.style.marginLeft = 0;
+}
+
+setTimeout(rideFromRight, 1000);
