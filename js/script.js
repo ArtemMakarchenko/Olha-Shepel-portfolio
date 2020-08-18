d642da30d0933e2217efc8aa99358784644061d5
makarich98 <artem1998makarchenko@gmail.com>
2020-08-18 21:10:36 +0300

    Added spawn animation for blocks

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-// let logoText = document.querySelector("h1");
 
-// function rideFromRight(){
-//     logoText.style.opacity = 1;
-//     logoText.style.marginLeft = 0;
-// }
-
-// setTimeout(rideFromRight, 1000);
