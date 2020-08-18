b0dfa5f9f29820c840a0bfe7c1887602dab14334
makarich98 <artem1998makarchenko@gmail.com>
2020-08-18 20:27:59 +0300

    Completed the creation of all pages and the stylistic design of the pages

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let logoText = document.querySelector("h1");
+// let logoText = document.querySelector("h1");
 
-function rideFromRight(){
-    logoText.style.opacity = 1;
-    logoText.style.marginLeft = 0;
-}
+// function rideFromRight(){
+//     logoText.style.opacity = 1;
+//     logoText.style.marginLeft = 0;
+// }
 
-setTimeout(rideFromRight, 1000);
+// setTimeout(rideFromRight, 1000);
